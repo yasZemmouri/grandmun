fa2da5c346f43761b71f86ebe61aaa6b0ae0cdf9
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2023-04-06 01:23:01 -0400

    rename misspelled functio name mobileNavToogle()

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,11 @@ console.log(mobileNavBtnEl);
 mobileNavBtnEl.forEach(element => {
     console.log(element);
     element.addEventListener('click', function () {
-        mobileNavToogle();
+        mobileNavToggle();
 
     })
 });
-function mobileNavToogle() {
+function mobileNavToggle() {
     mobileNavShowEl.classList.toggle('d-none');
     mobileNavHideEl.classList.toggle('d-none');
     // apply a screen that prevent scrolling and clicking on the body
@@ -26,13 +26,13 @@ document.querySelectorAll('.navbar a').forEach(navbarlink => {
     //2. if a link doesn't have a hash exit the function and check the next one.
     // 3. if you find a hash check if there is an element that has that hash 
     // 4. if you don't find it break 
-    // 5. if you find it add a click event listner to it. when clicked check if mobile-nav-active is on and do the same you do when you change page. toggle the nav hide show button and remove the mobile-nav-active screen
+    // 5. if you find it add a click event listener to it. when clicked check if mobile-nav-active is on and do the same you do when you change page. toggle the nav hide show button and remove the mobile-nav-active screen
     if (!navbarlink.hash) return;
     let section = document.querySelector(navbarlink.hash);
     if (!section) return;
     navbarlink.addEventListener('click', () => {
         if (document.querySelector('.mobile-nav-active')) {
-            mobileNavToogle();
+            mobileNavToggle();
         }
     })
 
