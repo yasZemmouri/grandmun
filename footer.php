5358252ac8218c0451433c0f7c819fe3f5a59c02
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2023-04-16 02:09:08 -0400

    $1

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
-    <div class="container-sm d-flex justify-content-around flex-nowrap">
-        <div>GrandMUN, <a href="https://www.nd-grandchamp.fr/"> Lycée Notre-Dame du Grandchamp</a></div>
-        <div>Site designed by <a href="https://algofairy.com/"> AlgoFairy.com</a></div>
+    <div class="container-xl d-flex justify-content-around align-items-center flex-wrap item gap-2">
+        <div class="flex-shrink-0">GrandMUN, <a href="https://www.nd-grandchamp.fr/" title="Visit ycée Notre-Dame du Grandchamp site" target='_blank'> Lycée Notre-Dame du Grandchamp</a></div>
+        <div class="flex-shrink-0">Site designed by <a href="https://algofairy.com/" title="Vist algofairy site" target='_blank'> AlgoFairy.com</a></div>
     </div>
 </footer>
